1fa2f4b1c995a29b14ed66efcbe4ad64e27008aa
SoftechIn <sathyasoftechin@gmail.com>
2024-05-02 16:00:16 +0530

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SR-Media-Frontend'`, () => {
+  it(`should have as title 'SR-Media'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SR-Media-Frontend');
+    expect(app.title).toEqual('SR-Media');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SR-Media-Frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('SR-Media app is running!');
   });
 });
